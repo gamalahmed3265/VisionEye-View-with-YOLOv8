7aafe29c3969ebbb9325d48455632dc7f1198a9a
Gamal Ahmed <gamalahmed3265@gmail.com>
2024-02-17 17:33:25 +0200

    Annotator , predict the boxes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import cv2
 
 
-from ultralytics import YOLO
+from ultralytics import YOLO,YOLOWorld
 from ultralytics.utils.plotting import colors,Annotator
 
 model=YOLO("yolov8n.pt")
+model=YOLOWorld("yolov8s-world.pt")
 names=model.model.names
 
 
@@ -16,26 +17,48 @@ def rescale_frame(frame, percent=75):
 
 
 
-vidoeUrl="..."
+url=r"C:\Projects\ccomputer vision\Computer-Vision-labs\70.Image Augmentation with Imgaug\assets"
+vidoeUrl=f'{url}/Vehicle detection and counting.mp4'
+
+# vidoeUrl="..."
 cap=cv2.VideoCapture(vidoeUrl)
 
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
+# out = cv2.VideoWriter('visioneye-pinpoint.avi', cv2.VideoWriter_fourcc(*'MJPG'), fps, (w, h))
 
 
+center_point = (-10, h)
 
 while cap.isOpened():
     ret,frame=cap.read()
     if not ret:
         break
     frame=rescale_frame(frame,percent=50)
+    
+    results=model.predict(frame)
+    print("^"*40)
+    print(results[0].boxes)
+    print("^"*40)
+    boxes =results[0].boxes.xyxy.cpu()
+    print("results XYXY",boxes )
+    clss=results[0].boxes.cls.cpu()
+    print("CLASS",clss)
+    conf=results[0].boxes.conf.cpu()
+    print("conf:",conf)
+    
+    annotator=Annotator(frame,line_width=2)
+    for box, cls in zip(boxes, clss):
+        annotator.box_label(box,confs=9 ,label=names[int(cls)], color=colors(int(cls)))
+        annotator.visioneye(box, center_point)
+
     cv2.imshow("visioneye-pinpoint",frame)
     
     key=cv2.waitKey(1)
     if key==ord("q"):
         break
 
-cv2.release()
+cap.release()
 cv2.destroyAllWindows()
 
 
