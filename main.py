fc961a32285d4df3cb1049501c7c1a3f8c3d19be
Gamal Ahmed <gamalahmed3265@gmail.com>
2024-02-17 16:33:27 +0200

    add fuctions rescale_frame to rescale frame with screen

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import cv2
 
-vidoeUrl="..."
-video=cv2.VideoCapture(vidoeUrl)
 
-while video.isOpened():
-    ret,cap=video.read()
+from ultralytics import YOLO
+from ultralytics.utils.plotting import colors,Annotator
+
+model=YOLO("yolov8n.pt")
+names=model.model.names
+
+
+def rescale_frame(frame, percent=75):
+    width = int(frame.shape[1] * percent / 100)
+    height = int(frame.shape[0] * percent / 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+
+
+vidoeUrl="..."
+cap=cv2.VideoCapture(vidoeUrl)
+
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+
+
+
+while cap.isOpened():
+    ret,frame=cap.read()
     if not ret:
         break
-    
-    cv2.imshow("visioneye-pinpoint",cap)
+    frame=rescale_frame(frame,percent=50)
+    cv2.imshow("visioneye-pinpoint",frame)
     
     key=cv2.waitKey(1)
     if key==ord("q"):
@@ -16,3 +37,5 @@ while video.isOpened():
 
 cv2.release()
 cv2.destroyAllWindows()
+
+
